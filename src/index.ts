7f777b1c5ca4de083cf0d91f492a9af473563b59
Saanicc <mattias470@gmail.com>
2025-10-22 03:55:37 +0200

    Connect mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import mongoose from "mongoose";
 import { registerDiscordClient } from "./bot/registerClient.js";
 import { registerPlayer } from "./bot/registerPlayer.js";
 import { config } from "./config.js";
 
 const setupBot = async () => {
+  mongoose
+    .connect(config.MONGO_URI)
+    .then(() => {
+      console.log("Connected to Mongo DB");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error: ", err);
+    });
+
   const client = registerDiscordClient();
   registerPlayer(client);
   client.login(config.DISCORD_TOKEN);
