95a67c0fb496d0a8b800ea0d0402e8204c520277
Saanicc <mattias470@gmail.com>
2025-11-02 21:41:47 +0100

    Add 'discord-player-youtube' package and remove local files

--- src/bot/registerPlayerExtractors.ts
+++ src/bot/registerPlayerExtractors.ts
@@ -2,41 +2,16 @@ import { Player } from "discord-player";
 import { AttachmentExtractor } from "@discord-player/extractor";
 import { SpotifyExtractor } from "discord-player-spotify";
 import { SoundcloudExtractor } from "discord-player-soundcloud";
-import { YoutubeSabrExtractor } from "../youtubei/youtubeiExtractor";
 import { config } from "../config";
+import { YoutubeExtractor } from "discord-player-youtube";
 
 export const registerPlayerExtractors = async (player: Player) => {
-  const spotifyExt = await player.extractors.register(SpotifyExtractor, {
+  await player.extractors.register(SpotifyExtractor, {
     clientId: config.SPOTIFY_CLIENT_ID,
     clientSecret: config.SPOTIFY_CLIENT_SECRET,
     market: "SE",
   });
-  const soundcloudExt = await player.extractors.register(
-    SoundcloudExtractor,
-    {}
-  );
-  const ytExt = await player.extractors.register(YoutubeSabrExtractor, {
-    cookies: config.YOUTUBE_COOKIE,
-    logSabrEvents: false,
-  });
-  const attatchmentExt = await player.extractors.register(
-    AttachmentExtractor,
-    {}
-  );
-
-  if (spotifyExt) {
-    spotifyExt.priority = 2;
-  }
-
-  if (soundcloudExt) {
-    soundcloudExt.priority = 1;
-  }
-
-  if (ytExt) {
-    ytExt.priority = 3;
-  }
-
-  if (attatchmentExt) {
-    attatchmentExt.priority = 0;
-  }
+  await player.extractors.register(YoutubeExtractor, {});
+  await player.extractors.register(SoundcloudExtractor, {});
+  await player.extractors.register(AttachmentExtractor, {});
 };
