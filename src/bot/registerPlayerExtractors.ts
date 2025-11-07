8a59ecf88d0d9894a533e4b900f7f3bd115959c9
Saanicc <mattias470@gmail.com>
2025-11-07 20:40:17 +0100

    Update discord-player-youtube and extractor priority

--- src/bot/registerPlayerExtractors.ts
+++ src/bot/registerPlayerExtractors.ts
@@ -1,17 +1,24 @@
 import { Player } from "discord-player";
-import { AttachmentExtractor } from "@discord-player/extractor";
 import { SpotifyExtractor } from "discord-player-spotify";
 import { SoundcloudExtractor } from "discord-player-soundcloud";
 import { config } from "../config";
 import { YoutubeExtractor } from "discord-player-youtube";
 
 export const registerPlayerExtractors = async (player: Player) => {
-  await player.extractors.register(SpotifyExtractor, {
+  const spotifyExt = await player.extractors.register(SpotifyExtractor, {
     clientId: config.SPOTIFY_CLIENT_ID,
     clientSecret: config.SPOTIFY_CLIENT_SECRET,
     market: "SE",
   });
-  await player.extractors.register(YoutubeExtractor, {});
-  await player.extractors.register(SoundcloudExtractor, {});
-  await player.extractors.register(AttachmentExtractor, {});
+  const youtubeExt = await player.extractors.register(YoutubeExtractor, {
+    cookie: config.YOUTUBE_COOKIE,
+  });
+  const soundcloudExt = await player.extractors.register(
+    SoundcloudExtractor,
+    {}
+  );
+
+  if (spotifyExt) spotifyExt.priority = 3;
+  if (youtubeExt) youtubeExt.priority = 2;
+  if (soundcloudExt) soundcloudExt.priority = 1;
 };
